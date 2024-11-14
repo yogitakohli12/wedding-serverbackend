5676716d3f45135c77849439506b18a4101b067a
yogita <yogikohli7100@gmail.com>
2024-11-14 18:31:29 +0530

    gfty

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const allowedOrigins = [
